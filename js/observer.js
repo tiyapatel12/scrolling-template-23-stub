411856dcc6cdfe5bfc4cdd0f6f36846e81736363
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2023-10-17 12:23:07 +0100

    import script

--- js/observer.js
+++ js/observer.js
@@ -1,7 +1,31 @@
+let mainDiv = document.getElementById("scrollArea");
+
+console.log(mainDiv)
+
 let options = {
-    root: document.getElementById("scrollArea"),
-    rootMargin: "0px",
-    threshold: 1.0,
-  };
-  
-  let observer = new IntersectionObserver(callback, options);
+  root: mainDiv,
+  rootMargin: "0px",
+  threshold: 1.0,
+};
+
+let callback = (entries, observer) => {
+  console.log("callback of observer");
+  entries.forEach((entry) => {
+    console.log("entry observed", entry);
+    console.log(entry.isIntersecting)
+    // Each entry describes an intersection change for one observed
+    // target element:
+    //   entry.boundingClientRect
+    //   entry.intersectionRatio
+    //   entry.intersectionRect
+    //   entry.isIntersecting
+    //   entry.rootBounds
+    //   entry.target
+    //   entry.time
+  });
+};
+
+let observer = new IntersectionObserver(callback, options);
+
+let target = document.getElementById("listItem");
+observer.observe(target);
