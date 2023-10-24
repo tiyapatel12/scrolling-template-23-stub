716cf6c5dc592677f8216001cf197d8c0ab6d53a
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2023-10-24 10:28:15 +0100

    comment javascript

--- js/observer.js
+++ js/observer.js
@@ -1,25 +1,43 @@
-let mainDiv = document.getElementById("scrollArea");
-
 let options = {
   rootMargin: "0px",
-  //threshold before intersecting returns true (1 is full size). If zero will be intersecting when they are touching but not seeing it.
+  // threshold before intersecting returns true (1 is full size). If zero will be intersecting when they are touching but not seeing it
   threshold: 0.1,
 };
 
-let callback = (entries, _observer) => {
-  entries.forEach((entry) => {
+/**
+ * Callback that is fired every time the viewport moves
+ * @param {Array<HTMLElement>} entries all elements that the observer is watching
+ * @param {HTMLElement} _observer root observer element - in this case the viewport
+ */
+const callback = (entries, _observer) => {
+  // loop through entries. Each entry has information linked to the element that it refers to
+  for (entry of entries) {
+    //get element from entry as we'll be modifying it's class
     const element = entry.target;
+
+    // entry.isIntersecting tells us if this particular entry is intersecting with the viewport
+
+    // if intersecting -> let css know this element is in view
     if (entry.isIntersecting) {
       element.classList.add("intersecting");
       return;
     }
 
+    // if not intersecting -> let css know this element is out of view
     element.classList.remove("intersecting");
-  });
+  }
 };
 
-let observer = new IntersectionObserver(callback, options);
+/**
+ * define observer object -> this object is in charge of watching out for intersections
+ * As you can see we are giving it two elements:
+ * 1. - The callback to know what it needs to do when an intersection happens
+ * 2. - The options defining rootMargin and threshold which define what an intersection means for us
+ */
+const observer = new IntersectionObserver(callback, options);
 
-let target = document.getElementById("listItem");
+// define the elements that we want to observe against the viewport
+const target = document.getElementById("listItem");
 
+// Let the observer know we want to start observing right away
 observer.observe(target);
