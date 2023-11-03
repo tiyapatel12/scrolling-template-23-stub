40a4276d8fafcbb65bd5e4260b035763fd73a1fa
DianaVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2023-11-03 16:40:46 +0000

    final product done

---
+++ js/audio.js
@@ -0,0 +1,10 @@
+const audio = new Audio('sound/scott_opening.mp3');
+const startAudioButton = document.getElementById('start-audio');
+
+/**
+ * When button is clicked, play audio
+ * @param {Event} _event 
+ */
+startAudioButton.onclick = (_event) => {
+  audio.play();
+}
